0cfd95fd40cde81a9c507f002a25590b7d1c63bd
David Williams <thedavesignal@gmail.com>
2019-04-12 00:26:42 -0700

    home-wed apr 11 (validation/sanitization for score controller)

--- api/src/controllers/score.controller.js
+++ api/src/controllers/score.controller.js
@@ -1,4 +1,5 @@
 import Score from '../models/score.model.js';
+import { body, validationResult } from 'express-validator/check';
 
 let ScoreController = {}
 
@@ -6,28 +7,42 @@ ScoreController.test = (req, res) => {
   return res.send('ScoreController');
 };
 
-ScoreController.addScore = (req, res) => {
+ScoreController.validate = (method) => {
+  switch (method) {
+    case 'addScore':
+      return [
+        body('name').trim().escape(),
+        body('time', 'Time must be numeric (in seconds)').isNumeric().trim().escape()
+      ]
+  }
+};
+
+ScoreController.addScore = (req, res, next) => {
+  const name = req.body.name;
+  const time = req.body.time;
   let score = new Score(
     {
-      name: req.body.name || 'anonymous',
-      time: req.body.time
+      name: name || 'anonymous',
+      time: time
     }
   );
+  const errors = validationResult(req);
 
   score.save(function (err) {
+    if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
     if (err) return next(err);
     res.send('Score recorded');
   })
 };
 
-ScoreController.getScore = (req, res) => {
+ScoreController.getScore = (req, res, next) => {
   Score.findById(req.params.id, (err, score) => {
     if (err) return next(err);
     res.send(score);
   })
 }
 
-ScoreController.getAllScores = (req, res) => {
+ScoreController.getAllScores = (req, res, next) => {
   Score.find({}, (err, scores) => {
     if (err) return next(err);
     res.send(scores);
