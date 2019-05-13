1ac2c647afa8a1cb824da4a5c34e71f178f5a42a
David Williams <thedavesignal@gmail.com>
2019-05-12 23:09:48 -0700

    hpc-may 12/3

--- api/src/controllers/score.controller.js
+++ api/src/controllers/score.controller.js
@@ -16,6 +16,10 @@ ScoreController.validate = (method) => {
         body('name').trim().escape(),
         body('time', 'Time must be numeric (in seconds)').isNumeric().trim().escape()
       ]
+    case 'updateName':
+      return [
+        body('name').trim().escape(),
+      ]
   }
 };
 
@@ -53,5 +57,20 @@ ScoreController.getAllScores = (req, res, next) => {
     res.send(scores);
   })
 };
+
+// Update
+ScoreController.updateName = (req, res, next) => {
+  Score.findById(req.params.id, (err, score) => {
+    if (err) return next(err);
+    score.name = req.body.name;
+    const errors = validationResult(req);
+    
+    score.save(function (err) {
+      if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
+      if (err) return next(err);
+      res.send(score);
+    })
+  })
+}
 
 export default ScoreController;
