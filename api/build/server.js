371ce154c73a65c14a3b3fa75eab48d0106ce95b
David Williams <thedavesignal@gmail.com>
2019-05-14 22:30:45 -0700

    Updates .gitignore to exclude API build folder

--- api/build/server.js
+++ api/build/server.js
@@ -1,13 +1,45 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
+var _express = _interopRequireDefault(require("express"));
 
-var test = function test() {
-  console.log('Okay.');
-};
+var _mongoose = _interopRequireDefault(require("mongoose"));
 
-var _default = test;
-exports["default"] = _default;
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+var _bodyParser = _interopRequireDefault(require("body-parser"));
+
+var _score = _interopRequireDefault(require("./routes/score.route"));
+
+var _target = _interopRequireDefault(require("./routes/target.route"));
+
+var _cors = _interopRequireDefault(require("cors"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var app = (0, _express["default"])();
+app.use((0, _cors["default"])());
+
+_dotenv["default"].config(); // Mongoose for MongoDB connection
+
+
+var mongoDB = process.env.DATABASE_URL;
+
+_mongoose["default"].connect(mongoDB);
+
+_mongoose["default"].Promise = global.Promise;
+var db = _mongoose["default"].connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+app.use(_express["default"].json());
+app.use(_bodyParser["default"].json());
+app.use(_bodyParser["default"].urlencoded({
+  extended: false
+})); // Real basic landing page
+
+app.get('/', function (req, res) {
+  return res.status(200).send('Okay');
+}); // Routers for Score and Target end points
+
+app.use('/score', _score["default"]);
+app.use('/target', _target["default"]);
+app.listen(4500);
+console.log('API running on port 4500.');
